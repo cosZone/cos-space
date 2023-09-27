0abd2b03b97291fc852d13561298c699c9631f96
yusixian <3450414733@qq.com>
2023-09-28 00:39:21 +0800

    fix: tailwind app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: 'class', // https://tailwindcss.com/docs/dark-mode
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/hooks/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/hooks/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       container: {
