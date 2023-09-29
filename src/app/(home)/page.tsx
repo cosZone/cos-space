b4754759192e170d5a8492757784fadaf7903ec3
yusixian <3450414733@qq.com>
2023-09-29 15:21:44 +0800

    feat: sider avatar & header ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,28 +1,9 @@
 'use client';
 import PostItemCard from '@/components/post/PostItemCard';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import _ from 'lodash-es';
+
+const posts = _.range(0, 10);
 
 export default function Home() {
-  return (
-    <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-wrap gap-2">
-          <Button>Default</Button>
-          <Button variant={'secondary'}>Secondary</Button>
-          <Button variant={'destructive'}>Destructive</Button>
-          <Button variant={'ghost'}>Ghost</Button>
-          <Button variant={'link'}>Link</Button>
-          <Button variant={'outline'}>Outline</Button>
-        </CardContent>
-        <CardFooter className="flex justify-between">Footer</CardFooter>
-      </Card>
-      <PostItemCard />
-      <PostItemCard />
-    </>
-  );
+  return <>{posts?.length ? posts.map((v, i) => <PostItemCard key={i} />) : null}</>;
 }
--- src/components/layout/sider.tsx
+++ src/components/layout/sider.tsx
@@ -7,6 +7,8 @@ import { useAtom } from 'jotai';
 import { oneLevelMenuExpandAtom, oneLevelTabSelectIdxAtom } from '@/store/app';
 import { useRouter } from 'next/navigation';
 import Drawer from '../ui/drawer';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { CardDescription } from '../ui/card';
 
 type SiderProps = {
   bottomItems: (NavItemProps & { key?: string })[];
@@ -24,22 +26,34 @@ const Sider = ({ bottomItems }: SiderProps) => {
       open={mobileExpand}
       onOpenChange={(open) => setMobileExpand(open)}
       render={() => (
-        <div className="flex h-full flex-col justify-between gap-2 p-2">
-          <div className="flex flex-col gap-1">
-            {routers.map(({ name, path, key }, idx) => (
-              <NavItem
-                key={key ?? name}
-                selected={selectIdx1 === idx}
-                className="w-full px-1 py-2"
-                onClick={() => {
-                  router.push(path);
-                  setSelectIdx1(idx);
-                  setMobileExpand(false);
-                }}
-                name={name}
-                indicatorClass="inset-x-4"
-              />
-            ))}
+        <div className="flex h-full max-w-xs flex-col justify-between gap-2 p-2">
+          <div className="flex-center-y gap-1">
+            <div className="flex-center-y mb-2 gap-1 px-2">
+              <Avatar className="h-40 w-40 shadow-card">
+                <AvatarImage src="https://ysx.cosine.ren/img/avatar.jpg" />
+                <AvatarFallback>C</AvatarFallback>
+              </Avatar>
+              <h1 className="font-candy text-3xl text-primary">Cos</h1>
+              <CardDescription className="line-clamp-3 max-h-15 overflow-hidden whitespace-pre-wrap">
+                {`图片迁移完毕，博客待重构～xxxxxx xxxxxxxxxx xxxxxxxxxxxxxxxx\n换行测试xxxxx xxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxx xxxxxxxxxx`}
+              </CardDescription>
+            </div>
+            <div className="w-full">
+              {routers.map(({ name, path, key }, idx) => (
+                <NavItem
+                  key={key ?? name}
+                  selected={selectIdx1 === idx}
+                  className="w-full px-1 py-2"
+                  onClick={() => {
+                    router.push(path);
+                    setSelectIdx1(idx);
+                    setMobileExpand(false);
+                  }}
+                  name={name}
+                  indicatorClass="inset-x-4"
+                />
+              ))}
+            </div>
           </div>
           <div className="flex flex-col gap-2">
             {bottomItems.map(({ key, icon, onClick }, idx) => (
