4b0ed1fc22e64262812107d35335f88df74354b2
yusixian <cosine_yu@qq.com>
2024-12-22 15:57:23 +0800

    build: prettier & docs & lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,14 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind({
+      // 允许在 Tailwind 的语法基础上编写嵌套的 CSS 声明
+      nesting: true,
+    }),
+  ],
+  devToolbar: {
+    enabled: true,
+  },
 });
