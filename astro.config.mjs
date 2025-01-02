ebe73a1b7dbdc855d8223fd85f825c09316be40c
yusixian <cosine_yu@qq.com>
2025-01-03 02:32:12 +0800

    feat: add posts pagination and redirect root to posts page

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,4 +20,8 @@ export default defineConfig({
   vite: {
     plugins: [svgr()],
   },
+
+  redirects: {
+    '/': '/posts',
+  },
 });
--- src/constants/router.ts
+++ src/constants/router.ts
@@ -10,11 +10,11 @@ export enum Routes {
   About = '/about',
   // Gallery = '/gallery',
   Post = '/post',
-  Dashboard = '/dashboard',
+  // Dashboard = '/dashboard',
 }
 export const routers: Router[] = [
   { name: '首页', path: Routes.Home },
   { name: '关于', path: Routes.About },
   // { name: '展示柜', path: Routes.Gallery },
-  { name: '仪表盘', path: Routes.Dashboard, needOwner: true },
+  // { name: '仪表盘', path: Routes.Dashboard, needOwner: true },
 ];
