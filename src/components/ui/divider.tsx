f3686492435ce8189c798fe5f7fe2df3868be814
yusixian <cosine_yu@qq.com>
2025-01-30 16:06:38 +0800

    fix: divider ui

--- src/components/ui/divider.tsx
+++ src/components/ui/divider.tsx
@@ -4,9 +4,9 @@ import type { ReactNode } from 'react';
 export default function Divider({ children, className }: { children?: ReactNode; className?: string }) {
   return (
     <div className={cn('flex items-center', className)}>
-      <span className="h-px flex-grow bg-muted-foreground/50" />
-      {children ? <h1 className="mx-4 text-2xl font-bold tracking-widest text-muted-foreground/50">{children}</h1> : null}
-      <span className="h-px flex-grow bg-muted-foreground/50" />
+      <span className="h-px flex-grow bg-foreground/40" />
+      {children ? <h1 className="mx-4 text-2xl font-bold tracking-widest text-foreground/40">{children}</h1> : null}
+      <span className="h-px flex-grow bg-foreground/50" />
     </div>
   );
 }
