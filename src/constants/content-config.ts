6d483aec52987a24cbab75b07cc798b419edf8a9
yusixian <i@cosine.ren>
2025-06-15 17:30:25 +0800

    feat: anchor smoothScroll config

--- src/constants/content-config.ts
+++ src/constants/content-config.ts
@@ -1,8 +1,11 @@
 export interface ContentConfig {
   // 是否为外部链接添加 target="_blank"
   addBlankTarget: boolean;
+  // 是否启用锚点链接的平滑滚动
+  smoothScroll: boolean;
 }
 
 export const defaultContentConfig: ContentConfig = {
   addBlankTarget: true,
+  smoothScroll: true,
 };
